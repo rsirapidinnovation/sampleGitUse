85efc115482695d0247a094a4fe8186aabf27c56
rsirapidinnovation <rsi@rapidinnovation.dev>
2022-05-04 17:59:01 +0530

    New Feature Branch

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(4000,(err)=>{
 if(err) console.log(err);
 else{
-    console.log("Connect with port ${4000} ")
+    console.log("Connect with port ${4000} ");
 }
 });
 
